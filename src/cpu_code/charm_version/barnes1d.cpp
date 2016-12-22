29d764ce59726568c23b83e62ade40bde57fca9b
Nitin Bhat <nbhat4@illinois.edu>
2016-12-22 07:30:47 -0600

    Fixed a small bug in the charm version, cleanup and comments
    Change-Id: Ib6a0ad45137c784dd9cfb407f9596cb61e69c6d4

--- src/cpu_code/charm_version/barnes1d.cpp
+++ src/cpu_code/charm_version/barnes1d.cpp
@@ -6,39 +6,50 @@ using namespace std;
 #include "../../barnes1d.h"
 #include "barnes.decl.h"
 
-#define DEBUG(x) 
+//define DEBUG(x) to x if you need to print out a lot of statements
+#define DEBUG(x)
 
 /* readonly */ CProxy_Main mainProxy;
 /* readonly */ CProxy_BarnesTreePiece tpProxy;
 
+/* Trivial Barnes Tree piece
+ * Each Treepiece stores a single tree node (either internal node or leaf)
+ */
 class BarnesTreePiece : public CBase_BarnesTreePiece {
   public:
+    //Tree node (1 node for 1 tree piece)
     BarnesNodeData node;
+
+    //Consumer (only initialized for a leaf)
     BarnesConsumer<BarnesTreePiece, BarnesKey> *cons;
-    //std::vector<BarnesNodeData> nodes;
+
+    //index of the first leaf
     BarnesKey firstLeaf;
+
+    //size of the tree
     BarnesKey treeSize;
+
+    //counter to keep a track of number of remote node requests sent
     int remoteCounter = 0;
-   
-    /*
-    BarnesTreePiece(std::vector<BarnesNodeData> tpnodes) {
-      nodes.insert(nodes.end(), tpnodes.begin(), tpnodes.end())
-    }
-    */
+
     BarnesTreePiece(BarnesNodeData tpnode, BarnesKey firstLeaf, BarnesKey treeSize) : node(tpnode), firstLeaf(firstLeaf), treeSize(treeSize) {
+      //create a constructor only if the node is a leaf
       if (thisIndex >= firstLeaf)
         cons = new BarnesConsumer<BarnesTreePiece, BarnesKey>(*this, node);
     }
 
+    //Check if all remote requests have completed
     void checkDone() {
       if (remoteCounter == 0) {
         DEBUG(CkPrintf("[%d]remoteCounter == 0\n", thisIndex);)
-        if (thisIndex >= firstLeaf)
-          DEBUG(CkPrintf("[%d]acc: %f\n", thisIndex, cons->acc);)
+        if (thisIndex >= firstLeaf){
+          CkPrintf("[%d] Acceleration of particle : %f\n", thisIndex, cons->acc);
+        }
         contribute(CkCallback(CkReductionTarget(Main, done), mainProxy));
       }
     }
 
+    //Begin computation
     void startWork() {
       DEBUG(CkPrintf("[%d]startWork()\n", thisIndex);)
       remoteCounter = 0;
@@ -49,22 +60,26 @@ class BarnesTreePiece : public CBase_BarnesTreePiece {
 
     BarnesTreePiece(CkMigrateMessage *m) {}
 
+    //Method called to request for a node
     template <class Consumer>
     void requestNode(BarnesKey bk, Consumer &c) {
+
       if (bk < 1 || bk >= treeSize)
         CkPrintf("BarnesParaTree: Requested INVALID tree node %d\n", (int)bk);
-		  else if (bk == thisIndex) {
-			  if (bk >= firstLeaf)
-				  c.consumeLocalLeaf(node, bk);
-			  else
-				  c.consumeLocalNode(node, bk);
-		  }
-      else {
+      else if (bk == thisIndex) {   //Local node
+        if (bk >= firstLeaf)
+          c.consumeLocalLeaf(node, bk);  //call consumer's leaf method
+        else
+          c.consumeLocalNode(node, bk);  //call consumer's node method
+      }
+      else { //remote node
         remoteCounter++;
+        //send a remote node request
         thisProxy[bk].requestRemoteNode(bk, thisIndex);
       }
     }
 
+    //Entry method called to request for a remote node
     void requestRemoteNode(BarnesKey bk, int consIndex) {
       if (bk >= firstLeaf)
         thisProxy[consIndex].consumeRemoteLeaf(node, bk);
@@ -72,15 +87,17 @@ class BarnesTreePiece : public CBase_BarnesTreePiece {
         thisProxy[consIndex].consumeRemoteNode(node, bk);
     }
 
+    //Entry method called to respond to a remote node request which is an internal node
     void consumeRemoteNode(const BarnesNodeData &n, const BarnesKey &key) {
       remoteCounter--;
-      cons->consumeLocalNode(n, key);
+      cons->consumeLocalNode(n, key); //call consumer's node method now that remote node is available
       checkDone();
     }
 
+    //Entry method called to respond to a remote node request which is a leaf node
     void consumeRemoteLeaf(const BarnesLeafData &n, const BarnesKey &key) {
       remoteCounter--;
-      cons->consumeLocalLeaf(n, key);
+      cons->consumeLocalLeaf(n, key); //call consumer's leaf method now that remote leaf is available
       checkDone();
     }
 };
@@ -91,7 +108,7 @@ class Main : public CBase_Main {
     BarnesKey treeSize, treeRoot, firstLeaf;
 
   Main(CkArgMsg *m) {
-    int depth = 20;
+    int depth = 3;
     treeSize = (BarnesKey)pow(2, depth);
     treeRoot = 1;
     firstLeaf = pow(2, depth-1);
@@ -102,9 +119,11 @@ class Main : public CBase_Main {
     mainProxy = thisProxy;
     tpProxy = CProxy_BarnesTreePiece::ckNew();
 
+    //Dynamic insertion of treepieces
     for (int i = 1; i < treeSize; i++) {
       tpProxy[i].insert(tree[i], firstLeaf, treeSize);
     }
+    //finish insertion
     tpProxy.doneInserting();
     CkPrintf("Main inserted tp array\n");
 
@@ -113,11 +132,13 @@ class Main : public CBase_Main {
 
   Main(CkMigrateMessage *m){}
 
+  //Method called on reduction
   void done() {
     CkPrintf("DONE!!!\n");
     CkExit();
   }
 
+  //Construct the tree in an array form recursively
   void constructNode(int index, float xMin, float xMax) {
     ///interior node
     if (2*index<treeSize) {
